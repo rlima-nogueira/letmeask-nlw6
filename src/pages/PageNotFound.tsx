b8872eedbbd09fb39d813d3636824ec0921d830b
Rafaela Lima Nogueira <rafaela.nogueira@meta.com.br>
2021-06-23 23:27:19 -0300

    Inital commit

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import '../styles/pagenotfound.scss'
+export function PageNotFound() {
+    return (
+        <div className="page-not-found">
+            <h1>Página não encontrada </h1>
+
+            <Link to="/">
+                Voltar para HOME
+            </Link>
+        </div>
+
+    );
+}
