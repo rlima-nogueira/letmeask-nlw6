04d1971ff49cc9e34a455b4533bed3cea7222ee6
Rafaela Lima Nogueira <rafaela.nogueira@meta.com.br>
2021-06-27 18:06:55 -0300

    pagina admin

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 export default function Routes() {
   return (
@@ -11,6 +12,9 @@ export default function Routes() {
             <Route exact path="/" component={Home} />
             <Route path="/rooms/new" component={NewRoom} />
             <Route path="/rooms/:id" component={Room} />
+
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
+
         </Switch>
     </Router>
   );
