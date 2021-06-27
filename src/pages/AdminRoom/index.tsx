0e45fdda78929e881b8054371bfd831936bc7034
Rafaela Lima Nogueira <rafaela.nogueira@meta.com.br>
2021-06-27 18:55:15 -0300

    confirmação de delete

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -9,7 +9,10 @@ import { useRoom } from '../../hooks/useRoom';
 
 
 import logoImg from '../../assets/images/logo.svg';
+import deleteImg from '../../assets/images/delete.svg';
+
 import './styles.scss'
+import { database } from '../../services/firebase';
 
 type RoomParams = {
     id: string;
@@ -22,6 +25,12 @@ export function AdminRoom() {
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
   
     return (
        <div id="page-room">
@@ -48,7 +57,14 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Remover pergunta" />
+                                </button>
+                            </Question>
                         );
                     })}
                 </div>
